815d48ba870b8e5aa49e08345d9daac33cdeacb3
Fiona Weaver <fiona.l.weaver@gmail.com>
2020-11-11 14:15:06 -0700

    commit tree ff5a7cefb04c2a785970a798a7de8cb477e5b53b

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarfs)
+  dwarfs.map.with_index(1) do |name, index| 
+    puts "#{index}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planteer_calls)
+  planteer_calls.collect do |element|
+    element.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.each do |num_of_letters|
+    return true if num_of_letters.length > 4 
+  end
+  return false
 end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+def find_the_cheese(array)
+  array.each do |cheese|
+    if cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+      return cheese
+    end 
+  end 
+  nil
+end 
